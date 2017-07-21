5a2907b67ff2b2d6c7287867b1ee0defdf3eeec0
Matthew O'Gorman <mog@rldn.net>
2017-07-21 00:35:59 -0400

    set utf8 correctly in a pure shell

--- default.nix
+++ default.nix
@@ -2,12 +2,14 @@ with import <nixpkgs> {};
 stdenv.mkDerivation rec {
   name = "env";
   env = buildEnv { name = name; paths = buildInputs; };
+  LANG = "en_US.UTF-8";
   buildInputs = [
     git
     elixir
     nodejs
     stdenv
     zlib
+    glibcLocales
   ];
   pathsToLink = [ "/include" ];
 }
